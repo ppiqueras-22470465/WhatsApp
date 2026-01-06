d569378c040b52619995487c6b5a47abe574c939
ppiqueras-22470465 <22470465@live.uem.es>
2026-01-06 19:34:49 +0100

    WIP

--- proyect/Servidor/servidor.py
+++ proyect/Servidor/servidor.py
@@ -3,55 +3,95 @@ import threading
 import os
 
 HOST = "127.0.0.1"
+ARCHIVO_USUARIOS = "usuarios.txt"
 
 # --- FUNCIONES AUXILIARES (LÓGICA) ---
 
-def validar_login(datos):
-    """
-    Comprueba si el usuario y contraseña son correctos.
-    Formato recibido: LOGIN:USUARIO:CONTRASEÑA [cite: 9]
-    """
-    # TODO 1: Implementar lectura real de 'usuarios.txt'.
-    # - Abrir el archivo "usuarios.txt" en modo lectura ('r').
-    # - Recorrerlo línea por línea.
-    # - Hacer split(':') a cada línea para separar usuario y contraseña.
-    # - Comparar con las variables que vienen en 'datos'.
-    # - Si coincide, retornar True. Si termina el archivo sin coincidencia, False.
+def validar_login(usuario_recibido, clave_recibida):
+    try:
+        archivo = open(ARCHIVO_USUARIOS, "r")
+        lineas = archivo.readlines()
+        archivo.close()
 
-    datos = datos.split(":")
-    for linea in open("usuarios.txt", "r"):
-        usuario_archivo, contrasena_archivo = linea.strip().split(":")
-        if datos[1] == usuario_archivo and datos[2] == contrasena_archivo:
-            print(f"Validando credenciales: {datos}")
-            return True
-    return False
+        for linea in lineas:
+            linea = linea.strip() # Quito los espacios vacío
+            if linea: # Sí sigue habiendo línea
+                partes = linea.split(":") # Como está separado por : los leemos así
+                if len(partes) == 2: # Si el formato es válido
+                    if partes[0] == usuario_recibido and partes[1] == clave_recibida: # Sí coinciden usuario y contraseña
+                        return True
+            else:
+                print(f"[ERROR] No se encuentra el archivo {ARCHIVO_USUARIOS}")
+
+        return False
+    except FileNotFoundError:
+        print(f"[ERROR] No se encuentra el archivo {ARCHIVO_USUARIOS}")
+
 
 def guardar_mensaje_en_archivo(mensaje_formateado):
-    """
-    Recibe el string con ; y lo guarda en el archivo correspondiente.
-    Busca si existe Origen_Destino.txt o Destino_Origen.txt[cite: 67, 68].
-    """
-    # TODO 2: Lógica de selección de archivo de chat.
-    # - Extraer remitente y destinatario del 'mensaje_formateado' (split por ';').
-    # - Comprobar con os.path.exists() si existe "remitente_destinatario.txt".
-    # - Si no, comprobar si existe "destinatario_remitente.txt".
-    # - Si no existe ninguno, crear uno nuevo (por defecto "remitente_destinatario.txt").
-    
-    # TODO 3: Escritura física.
-    # - Abrir ese archivo en modo append ('a').
-    # - Escribir 'mensaje_formateado' seguido de un salto de línea (\n).
-    # - Cerrar archivo.
-    print(f"DISCO DURO >> Guardando mensaje: {mensaje_formateado}")
+    mensaje = mensaje_formateado.decode() # Cojo el mensaje del usuario
+    partes = mensaje.split(";") # Separo el mensaje que me envia por ;
+    if len(partes) >= 6: # El mensaje que se envia es [emisor;receptor;addrs;estado;mensaje]
+        emisor = partes[0] #.replace("@","") Por si crea mal los archivos
+        receptor = partes[1]
+        contenido_mensaje = partes[5]
+        print(f"Mensaje de {emisor} para {receptor}")
+
+        archivo = f"{emisor}_{receptor}.txt"
+        archivo_invertido = f"{receptor}_{emisor}.txt"
+
+        if os.path.exists(archivo): # - Comprobar con os.path.exists() si existe "remitente_destinatario.txt".
+            archivo_final = archivo
+        elif os.path.exists(archivo_invertido): # - Si no, comprobar si existe "destinatario_remitente.txt".
+            archivo_final = archivo_invertido
+        else:
+            archivo_final = archivo
+        try:
+            archivo_final_escribir = open(archivo_final, "a")
+            archivo_final_escribir.write(mensaje+"\n")
+            archivo_final_escribir.close()
+            print(f"[SISTEMA] Guardado en: {archivo_final}")
+        except Exception as e:
+            print(f"[ERROR] {e}")
+    else:
+        print(f"[ERROR] El mensaje recibido no tiene el formato correcto")
 
 # --- FUNCIONES NUEVAS NECESARIAS ---
 
-def obtener_mensajes_pendientes(usuario_solicitante, usuario_destino, ultimo_timestamp):
+def obtener_mensajes_pendientes(emisor, receptor, ultimo_timestamp):
     # TODO 4: Función para el UPDATE.
-    # - Localizar el archivo de chat entre estos dos usuarios.
-    # - Leer todas las líneas.
-    # - Filtrar: Quedarse solo con líneas cuyo timestamp (campo 3) > ultimo_timestamp.
-    # - Retornar esa lista de líneas.
-    pass
+    # Usamos la misma logíca para abrir el archivo
+    archivo = f"{emisor}_{receptor}.txt"
+    archivo_invertido = f"{receptor}_{emisor}.txt"
+
+    if os.path.exists(archivo):
+        archivo_final = archivo
+    elif os.path.exists(archivo_invertido):
+        archivo_final = archivo_invertido
+    else:
+        # Si no encontramos el archivo devolvemos vacío
+         return []
+    mensaje = []
+    try:
+        # Usamos el r para leer y seguimos la misma logica usada en validar_login
+        archivo_leer = open(archivo_final, "r")
+        lineas = archivo_leer.readlines()
+        archivo_leer.close()
+        for linea in lineas:
+            linea = linea.strip()
+            partes=linea.split(";")
+            if len(partes) >= 6:
+                timestamp_mensaje = partes[2]
+                # Comparó las cadenas
+                if timestamp_mensaje > ultimo_timestamp:
+                    mensaje.append(linea)
+    except FileNotFoundError:
+        return []  # Si falla devuelvo vacía
+    except Exception as e:
+        print(f"[ERROR LECTURA] {e}")
+        return []
+
+    return mensaje
 
 # --- HILOS DE CONEXIÓN ---
 
@@ -61,23 +101,24 @@ def puerto_666():
     servidor.bind((HOST, 666))
     servidor.listen()
     print("Servidor [666]: Listo para recibir mensajes.")
-    
+
     while True:
         conn, addr = servidor.accept()
         datos = conn.recv(1024).decode()
-        
+
         if datos:
             # Aquí llega el mensaje formateado: Origen;Destino;...
             print(f"[666] Recibido: {datos}")
-            
+
             # TODO 5: Cambiar estado a RECIBIDO.
             # - Antes de guardar, reemplazar en el string "ENVIADO" por "RECIBIDO"[cite: 79].
             # - Actualizar el timestamp de estado.
             guardar_mensaje_en_archivo(datos)
-            
+
+
             # Confirmamos recepción obligatoria [cite: 10]
             conn.send("OK".encode())
-            
+
         conn.close()
 
 def puerto_999():
@@ -86,22 +127,22 @@ def puerto_999():
     servidor.bind((HOST, 999))
     servidor.listen()
     print("Servidor [999]: Listo para Login y Updates.")
-    
+
     while True:
         conn, addr = servidor.accept()
         datos = conn.recv(1024).decode()
-        
+
         if datos.startswith("LOGIN"):
             # Gestión de Login 
             if validar_login(datos):
                 conn.send("OK".encode())
             else:
                 conn.send("KO".encode())
-                
+
         elif "UPDATE" in datos:
             # Petición de actualización de mensajes [cite: 105]
             print(f"[999] Petición UPDATE recibida: {datos}")
-            
+
             # TODO 6: Implementar protocolo de entrega de mensajes[cite: 123].
             # 1. Parsear 'datos' para sacar quién pide (Origen) y la fecha (Timestamp).
             # 2. Llamar a obtener_mensajes_pendientes().
@@ -110,9 +151,9 @@ def puerto_999():
             # 5. Bucle for mensaje in mensajes:
             #    - conn.send(mensaje)
             #    - conn.recv(1024) -> Esperar "OK" de cada mensaje.
-            
+
             conn.send("OK".encode()) # Esto es temporal, borrar al hacer el TODO 6
-            
+
         # TODO 7: Implementar LIST[cite: 108].
         # - Añadir un elif "LIST" in datos:
         # - Buscar todos los archivos donde aparezca el nombre del usuario.
